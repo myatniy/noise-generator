b30b00a871685a92d44a404b716b7ac5a9b55d1a
myatniy <ipsixin3d@gmail.com>
2019-06-12 02:04:12 +0300

    Mission accomplished

--- my-script.js
+++ my-script.js
@@ -50,3 +50,94 @@ function playSound(volumeId, imageId, playSoundId) {
     playAudio.pause();
   }
 }
+
+// ********** modal image **********
+
+function fullscreen(modalId, imgGalleryId, imgGalleryModalId, close) {
+  let modal = modalId;
+  let img = imgGalleryId;
+  let modalImg = imgGalleryModalId;
+  let span = close;
+
+  img.onclick = function() {
+    modal.style.display = 'block';
+    modalImg.src = this.src;
+  }
+
+  span.onclick = function() {
+    modal.style.display = 'none';
+  }
+}
+
+// ********** timer and shit **********
+let showReset = document.getElementById('reset');
+let showTime = document.getElementById('time');
+let setUpTimeButton = document.getElementById('setUpTimeButtonId');
+let timeInput = document.getElementById('timeId');
+
+function setUpTimer(timerTime) {
+  let time = timerTime.value;
+  let hh = parseInt(time.slice(0, 2));
+  let mm = parseInt(time.slice(3));
+
+  if (!(isNaN(hh) && isNaN(mm))) {
+    let timeParsed = hh * 60 * 60 + mm * 60;
+    let display = document.querySelector('#time');
+    startTimer(timeParsed, display);
+
+    timeInput.style.display = 'none';
+    setUpTimeButton.style.display = 'none';
+    showReset.style.display = 'inline';
+    showTime.style.display = 'inline';
+  } else {
+    alert('Неверно задано время, для того, чтобы избежать этой ошибки, окно ввода времени должно состоять из чисел.');
+  }
+}
+
+function startTimer(duration, display) {
+
+  reset.onclick = function () {
+    timeInput.style.display = 'inline';
+    setUpTimeButton.style.display = 'inline';
+    showReset.style.display = 'none';
+    showTime.style.display = 'none';
+
+    clearInterval(countdown);
+  }
+
+  let timer = duration; let hours; let minutes; let seconds; let m; let lastAction;
+  let countdown = setInterval(function () {
+    m = parseInt(timer / 60);
+    seconds = parseInt(timer % 60);
+    hours = parseInt(m / 60);
+    minutes = parseInt(m % 60);
+
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    display.innerText = hours + ':' + minutes + ':' + seconds;
+
+    if (--timer < 0) {
+      timer = duration;
+      console.log(timer);
+    }
+
+    lastAction = duration--;
+    console.log(lastAction);
+    if (lastAction === 0) {
+      const srcOfSpeakerId = document.getElementById('speakerId');
+      const audio = document.querySelectorAll('audio');
+
+      audio.forEach(elem => muteAudio(elem));
+      srcOfSpeakerId.src = 'volume-icons/speaker-off.png';
+
+      timeInput.style.display = 'inline';
+      setUpTimeButton.style.display = 'inline';
+      showReset.style.display = 'none';
+      showTime.style.display = 'none';
+
+      clearInterval(countdown);
+    }
+  }, 1000);
+}
